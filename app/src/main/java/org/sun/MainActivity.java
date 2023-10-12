754be170b290db836070f2d0cdb2e2e701f2a2a7
Sunnyof <522022923@qq.com>
2023-10-12 13:51:40 +0800

    620139 A包

---
+++ app/src/main/java/org/sun/MainActivity.java
@@ -0,0 +1,13 @@
+package org.sun;
+
+import android.os.Bundle;
+
+import com.cocos.lib.CocosActivity;
+
+public class MainActivity extends CocosActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
